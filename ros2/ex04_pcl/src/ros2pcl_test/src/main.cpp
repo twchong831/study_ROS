5a5815da57f7916c8fead913ca343b0840faa14d
twwt <71128226+twchong831@users.noreply.github.com>
2024-01-03 16:54:10 +0900

    update

--- ros2/ex04_pcl/src/ros2pcl_test/src/main.cpp
+++ ros2/ex04_pcl/src/ros2pcl_test/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <chrono>
 
 // PCL
 #include <pcl-1.12/pcl/point_types.h>
@@ -9,17 +10,27 @@
 
 // ROS2
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/PointCloud2.h"
 
 typedef pcl::PointCloud<pcl::PointXYZRGB>	PointCloudT;
 
 int main(int argc, char **argv)
 {
 	rclcpp::init(argc, argv);
-
 	RCLCPP_INFO(rclcpp::get_logger("ros2pclTest"), "Hello, World");
 
-	pcl::PointXYZRGB point;
+	rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
 
+	custom_qos_profile.depth = 1;
+	custom_qos_profile.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+	custom_qos_profile.history = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+
+	rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("ros2pclTest");
+	rclcpp::Publisher<sensor_msgs::PointCloud2>::SharedPtr pub_pointcloud;
+	pub_pointcloud = node->create_publisher<sensor_msgs::PointCloud2>("points2", custom_qos_profile);
+
+	// PCL point cloud
+	pcl::PointXYZRGB point;
 	PointCloudT::Ptr pointcloud(new PointCloudT);
 
 	for(uint i=0; i<100; i++)
@@ -35,7 +46,7 @@ int main(int argc, char **argv)
 	}
 
 	// pcl to ros2 msg
-	sensor_msgs::msg::PointCloud2 msg;
+	sensor_msgs::PointCloud2 msg;
 	pcl::toROSMsg(*pointcloud, msg);
 
 	RCLCPP_INFO(rclcpp::get_logger("ros2pclTest"), "Check Point CLoud Size : %d", pointcloud->size());
